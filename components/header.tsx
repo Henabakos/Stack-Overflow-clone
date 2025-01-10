c89c95e6027723a8b6a40439aff2a9785e4393c0
Henabakos <henogato9876@gmail.com>
2025-01-10 14:21:50 +0300

    fixed responsiveness for this stackoverflow clone

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ import { Menu } from "lucide-react";
 
 export function Header({ toggleSidebar }: { toggleSidebar: () => void }) {
   return (
-    <header className="w-full sticky top-0 z-50 px-10 shadow-sm shadow-[] bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="w-full sticky top-0 z-50 px-10 shadow-sm shadow-[#131722] bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
         <button
           onClick={toggleSidebar}
